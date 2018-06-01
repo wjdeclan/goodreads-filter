d4de91bffe3cbc4efb165fb6c662c358560da088
Declan Whitford Jones <wjdeclan@gmail.com>
2018-06-01 13:07:27 -0400

    Use storage.local for storing author array

--- filter.js
+++ filter.js
@@ -1,23 +1,33 @@
-var filteredAuthors = ["###"];
+function onGot(item) {
+  authorArray = item["goodreads-filter"].split(",").slice(0, -1);
 
-//get all spans with attribute itemprop="name"
-let spanList = document.querySelectorAll("span[itemprop=\"name\"]");
+	//get all spans with attribute itemprop="name"
+	let spanList = document.querySelectorAll("span[itemprop=\"name\"]");
 
-//iterate across them
-for (let name of spanList) {
+	//iterate across them
+	for (let name of spanList) {
 
-	//if their content matches a given name
-	if (filteredAuthors.indexOf(name.textContent) != -1) {
+		//if their content matches a given name
+		if (authorArray.indexOf(name.textContent) != -1) {
 
-		//find parent
-		let p = name.parentElement;
+			//find parent
+			let p = name.parentElement;
 
-		//iterate higher in the tree until reaching the relevant table row
-		while (p.tagName != "TR") {
-			p = p.parentElement;
+			//iterate higher in the tree until reaching the relevant table row
+			while (p.tagName != "TR") {
+				p = p.parentElement;
+			}
+
+			//and remove
+			p.remove();
 		}
-
-		//and remove
-		p.remove();
 	}
 }
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+var tfilteredAuthors = browser.storage.local.get("goodreads-filter");
+
+tfilteredAuthors.then(onGot, onError);
