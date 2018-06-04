829ab7a44b62ce158d8cd95b2eaf95dc7ee873b2
Declan Whitford Jones <wjdeclan@gmail.com>
2018-06-04 13:54:26 -0400

    Add button to author pages.

---
+++ author.js
@@ -0,0 +1,3 @@
+let box = document.querySelector(".authorFollowButtonContainer");
+
+box.innerHTML = box.innerHTML + "<br>" + "<button name=\"button\" type=\"button\" class=\"gr-button gr-button--fullWidth gr-button--togglable--disabled\">Block Author</button>"
