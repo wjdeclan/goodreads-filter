2b9d0a8bb97a8fc83892b0f594354df26502438e
Declan Whitford Jones <wjdeclan@gmail.com>
2018-06-04 14:14:28 -0400

    Basic button behavior

--- author.js
+++ author.js
@@ -10,9 +10,23 @@ function onGot(item) {
   let box = document.querySelector(".authorFollowButtonContainer");  
 
   if (authorArray.indexOf(name) != -1) {
-    box.innerHTML = box.innerHTML + "<br>" + "<button name=\"button\" type=\"button\" class=\"gr-button gr-button--fullWidth gr-button--togglable--disabled\">Unblock Author</button>"
+    box.innerHTML = box.innerHTML + "<br>" + "<button name=\"button\" type=\"button\" class=\"gr-button gr-button--fullWidth gr-button--togglable--disabled\" id=\"block-button\">Unblock Author</button>";
   } else {
-    box.innerHTML = box.innerHTML + "<br>" + "<button name=\"button\" type=\"button\" class=\"gr-button gr-button--fullWidth gr-button--togglable--disabled\">Block Author</button>"
+    box.innerHTML = box.innerHTML + "<br>" + "<button name=\"button\" type=\"button\" class=\"gr-button gr-button--fullWidth gr-button--togglable--disabled\" id=\"block-button\">Block Author</button>";
+  }
+
+  let btn = document.querySelector("#block-button");
+
+  btn.addEventListener("click", function() { btnClick(btn); });
+}
+
+function btnClick(btn) {
+  console.log("click");
+
+  if (btn.textContent === 'Block Author') {
+    btn.textContent = 'Unblock Author';
+  } else {
+    btn.textContent = 'Block Author';
   }
 }
 
